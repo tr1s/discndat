b17bfec79a1c974d2d378fb4f174f0edb5f8cce6
Tristan Lawrence <trislaw@gmail.com>
2021-02-10 21:51:09 -0500

    initial commit ⚡️

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>💽 Disc 'n Dat coming soon.</h1>
+    </div>
+  );
+}
